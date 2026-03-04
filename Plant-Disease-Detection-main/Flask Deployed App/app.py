55239abbf5318409a3087d5a1cc22a0888e1747a
Diinesh Polisetty <dineshpolisetty549@gmail.com>
2026-03-04 18:22:56 +0530

    Format code with black

--- Plant-Disease-Detection-main/Flask Deployed App/app.py
+++ Plant-Disease-Detection-main/Flask Deployed App/app.py
@@ -7,13 +7,13 @@ import numpy as np
 import torch
 import pandas as pd
 
+disease_info = pd.read_csv("disease_info.csv", encoding="cp1252")
+supplement_info = pd.read_csv("supplement_info.csv", encoding="cp1252")
 
-disease_info = pd.read_csv('disease_info.csv' , encoding='cp1252')
-supplement_info = pd.read_csv('supplement_info.csv',encoding='cp1252')
-
-model = CNN.CNN(39)    
+model = CNN.CNN(39)
 model.load_state_dict(torch.load("plant_disease_model_1_latest.pt"))
 model.eval()
+
 
 def prediction(image_path):
     image = Image.open(image_path)
@@ -28,45 +28,66 @@ def prediction(image_path):
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home_page():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact-us.html')
+    return render_template("contact-us.html")
 
-@app.route('/index')
+
+@app.route("/index")
 def ai_engine_page():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/mobile-device')
+
+@app.route("/mobile-device")
 def mobile_device_detected_page():
-    return render_template('mobile-device.html')
+    return render_template("mobile-device.html")
 
-@app.route('/submit', methods=['GET', 'POST'])
+
+@app.route("/submit", methods=["GET", "POST"])
 def submit():
-    if request.method == 'POST':
-        image = request.files['image']
+    if request.method == "POST":
+        image = request.files["image"]
         filename = image.filename
-        file_path = os.path.join('static/uploads', filename)
+        file_path = os.path.join("static/uploads", filename)
         image.save(file_path)
         print(file_path)
         pred = prediction(file_path)
-        title = disease_info['disease_name'][pred]
-        description =disease_info['description'][pred]
-        prevent = disease_info['Possible Steps'][pred]
-        image_url = disease_info['image_url'][pred]
-        supplement_name = supplement_info['supplement name'][pred]
-        supplement_image_url = supplement_info['supplement image'][pred]
-        supplement_buy_link = supplement_info['buy link'][pred]
-        return render_template('submit.html' , title = title , desc = description , prevent = prevent , 
-                               image_url = image_url , pred = pred ,sname = supplement_name , simage = supplement_image_url , buy_link = supplement_buy_link)
+        title = disease_info["disease_name"][pred]
+        description = disease_info["description"][pred]
+        prevent = disease_info["Possible Steps"][pred]
+        image_url = disease_info["image_url"][pred]
+        supplement_name = supplement_info["supplement name"][pred]
+        supplement_image_url = supplement_info["supplement image"][pred]
+        supplement_buy_link = supplement_info["buy link"][pred]
+        return render_template(
+            "submit.html",
+            title=title,
+            desc=description,
+            prevent=prevent,
+            image_url=image_url,
+            pred=pred,
+            sname=supplement_name,
+            simage=supplement_image_url,
+            buy_link=supplement_buy_link,
+        )
 
-@app.route('/market', methods=['GET', 'POST'])
+
+@app.route("/market", methods=["GET", "POST"])
 def market():
-    return render_template('market.html', supplement_image = list(supplement_info['supplement image']),
-                           supplement_name = list(supplement_info['supplement name']), disease = list(disease_info['disease_name']), buy = list(supplement_info['buy link']))
+    return render_template(
+        "market.html",
+        supplement_image=list(supplement_info["supplement image"]),
+        supplement_name=list(supplement_info["supplement name"]),
+        disease=list(disease_info["disease_name"]),
+        buy=list(supplement_info["buy link"]),
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
